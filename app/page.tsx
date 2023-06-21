e3828e3e0d9b8bf399441fca66fadda7b4d053dc
JAEHYU LEE <air-userplane@JAEHYUs-MacBook-Air.local>
2023-06-21 18:14:39 +0900

    .

--- app/page.tsx
+++ app/page.tsx
@@ -45,6 +45,23 @@ export default async function HomePage() {
         variant="secondary"
       />
 
+      <div className="min-h-screen items-center justify-center bg-sea1 bg-cover bg-fixed bg-center bg-no-repeat">
+        <h1 className="text-6xl font-black">Parallax</h1>
+      </div>
+
+      <h2 className="min-h-screen text-4xl font-bold">Sample Section</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam eveniet molestias
+        veniam, inventore sint voluptates nam provident impedit laborum ipsa vel quos amet
+        laboriosam dolorum suscipit! Nihil eos repellendus blanditiis repellat laboriosam veniam
+        quod maxime ab! Ea eveniet doloremque, excepturi totam, et molestias dicta accusamus
+        quibusdam quas sunt inventore!
+      </p>
+
+      <div className="min-h-screen bg-sea2 bg-cover bg-fixed bg-center bg-no-repeat"></div>
+
+      <div className="min-h-screen bg-sea3 bg-cover bg-fixed bg-no-repeat"></div>
+
       {/* @ts-expect-error Server Component */}
       <Footer />
     </>
